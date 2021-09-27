f1fe4c96548ce0a6e6ca1500f1d1ca008bdb2bcb
Lucas Fridez <lucas@fridez.dev>
2021-09-27 21:13:02 +0200

    Update: auto install script

--- auto-install.sh
+++ auto-install.sh
@@ -3,8 +3,8 @@
 rm -rf /tmp/linux-install
 mkdir /tmp/linux-install
 cd /tmp/linux-install
-curl -L https://gitlab.com/lucalaissue/linux-config/-/archive/master/linux-config-master.tar.gz | tar -xz
-cd linux-config-master
+curl -L https://github.com/fridezlucas/manjaro-config/archive/refs/heads/master.zip | unzip
+cd master
 sh install.sh
 cd ~
-rm -r /tmp/linux-install
+rm -r /tmp/master
