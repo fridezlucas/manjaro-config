5d34b1c69bbf80340e240256190e381ace2ceb55
Lucas Fridez <lucas@fridez.dev>
2021-09-27 21:04:31 +0200

    Add: gnome script (WIP)

---
+++ gnome/gnome.sh
@@ -0,0 +1,17 @@
+# Blur extension
+git clone https://github.com/ckissane/blur-me
+cd blur-me
+make install
+cd ..
+rm -rf blur-me
+
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable workspace-indicator@gnome-shell-extensions.gcampax.github.com
+
+gsettings set org.gnome.desktop.interface gtk-theme "todo"
+gsettings set org.gnome.shell.extensions.user-theme name "todo"
+gsettings set org.gnome.desktop.interface icon-theme "todo"
+gsettings set org.gnome.desktop.interface cursor-theme "volantes_cursors"
+
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
